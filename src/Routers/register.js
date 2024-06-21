f088856d9723f2064da6d090869837355bfadf39
XdMMD <102690832+MMDXd@users.noreply.github.com>
2024-06-21 20:07:15 +0430

    Update register.js

--- src/Routers/register.js
+++ src/Routers/register.js
@@ -6,14 +6,14 @@ const { User } = require("../db/schemas/userSchema")
 
 const registerFields = [
     body("email").isEmail(),
-    body("password"),
+    body("password").isString().notEmpty(),
     body("fullname").isString().notEmpty(),
     body("phonenumber").isMobilePhone("fa-IR").notEmpty(),
 ]
 
 Router.post("/", process.Session, registerFields, validateRequest, async (req, res) => {
     const {email, fullname, password} = req.body
-    if (await getUserDataByEmail(email).valid) {
+    if ((await getUserDataByEmail(email)).valid) {
         return res.status(400).json({message: "duplicate email"})
     }
     const user = new User({email, fullname, password})
